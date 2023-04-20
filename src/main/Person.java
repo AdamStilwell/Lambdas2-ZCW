e95e8587e8ec762cfcc165a126598e377c69844a
adams <stilwelladam8@gmail.com>
2023-04-20 17:44:49 -0400

    Made a test for the person tester.

--- src/main/Person.java
+++ src/main/Person.java
@@ -1,8 +1,19 @@
+import java.time.LocalDate;
+
 public class Person {
     String name;
+    LocalDate birthday;
     int age;
     Sex gender;
     String emailAddress;
+
+    public Person(String name, LocalDate birthday, int age, Sex gender, String emailAddress) {
+        this.name = name;
+        this.birthday = birthday;
+        this.age = age;
+        this.gender = gender;
+        this.emailAddress = emailAddress;
+    }
 
     public String getName() {
         return name;
@@ -10,6 +21,14 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
     }
 
     public int getAge() {
@@ -34,5 +53,23 @@ public class Person {
 
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthday=" + birthday +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", emailAddress='" + emailAddress + '\'' +
+                '}';
+    }
+
+    public String printPerson() {
+        System.out.println(this);
+        return toString();
     }
 }
--- src/test/TestLocal.java
+++ src/test/TestLocal.java
@@ -1,2 +1,29 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class TestLocal {
+    @Test
+    public void personCheckerTest(){
+        ArrayList<Person> people = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        Person p = new Person("Henry", null, 90, Sex.MALE, "madeInHell8@gmail.com");
+        Person p2 = new Person("Filson", null, 102, Sex.MALE, "madeInHelk13@aol.com");
+        Person p3 = new Person("Utopina", null, 103, Sex.FEMALE, "madeInHeaven@gmail.com");
+        people.add(p);
+        people.add(p2);
+        people.add(p3);
+
+        String expected = sb.append(p)
+                .append("\n")
+                .append(p2)
+                .append("\n")
+                .toString();
+
+        String actual = PersonService.printPersons(people, new PersonChecker());
+
+        Assert.assertEquals(expected, actual);
+
+    }
 }
