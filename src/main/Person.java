a736d53b3e1b9cb7170e3c0fd0dcbd58fa02f42a
adams <stilwelladam8@gmail.com>
2023-04-20 16:46:54 -0400

    Added the local branch.

---
+++ src/main/Person.java
@@ -0,0 +1,38 @@
+public class Person {
+    String name;
+    int age;
+    Sex gender;
+    String emailAddress;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Sex getGender() {
+        return gender;
+    }
+
+    public void setGender(Sex gender) {
+        this.gender = gender;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
